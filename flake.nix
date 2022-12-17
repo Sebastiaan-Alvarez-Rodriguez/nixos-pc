88f1d803d53a02960c01210b5b43f25adf9909a3
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2022-12-17 13:25:08 +0100

    WIP: Update configuration.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
-    nixos-hardware.url = "github:nixos/nixos-hardware"; # required for polonium
+    # nixos-hardware.url = "github:nixos/nixos-hardware"; # required for polonium
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
