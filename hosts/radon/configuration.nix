17708b9468587d7a2f5a7efd2f648f067cda1bfa
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2023-03-18 01:03:41 +0100

    SSH key configuration

--- hosts/radon/configuration.nix
+++ hosts/radon/configuration.nix
@@ -152,16 +152,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   environment.systemPackages = [
     pkgs.home-manager
