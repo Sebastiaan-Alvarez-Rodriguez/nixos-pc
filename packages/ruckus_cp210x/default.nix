b17ce8450e1c65f612aff7bf41eebec9dceeacbd
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-03-22 15:04:03 +0100

    WIP: add cp210x serial driver

--- packages/ruckus_cp210x/default.nix
+++ packages/ruckus_cp210x/default.nix
@@ -1,20 +1,20 @@
-{ lib, stdenv, kernel, bc }:
+{ pkgs, lib, stdenv, bc }:
 stdenv.mkDerivation rec {
   pname = "ruckus_cp210x";
-  version = "${kernel.version}-2024-03-22";
+  version = "${pkgs.linuxPackages.kernel.version}-2024-03-22";
 
   src = ./.;
 
   hardeningDisable = [ "pic" ];
 
-  nativeBuildInputs = [ bc ] ++ kernel.moduleBuildDependencies;
-  makeFlags = kernel.makeFlags;
+  nativeBuildInputs = [ bc ] ++ pkgs.linuxPackages.kernel.moduleBuildDependencies;
+  makeFlags = pkgs.linuxPackages.kernel.makeFlags;
   prePatch = ''
     substituteInPlace ./Makefile \
-      --replace /lib/modules/$(KVERSION) "$out/lib/modules/${kernel.modDirVersion}"
+      --replace /lib/modules/$(KVERSION) "$out/lib/modules/${pkgs.linuxPackages.kernel.modDirVersion}"
   '';
   preInstall = ''
-    mkdir -p "$out/lib/modules/${kernel.modDirVersion}"
+    mkdir -p "$out/lib/modules/${pkgs.linuxPackages.kernel.modDirVersion}"
   '';
   enableParallelBuilding = true;
 
