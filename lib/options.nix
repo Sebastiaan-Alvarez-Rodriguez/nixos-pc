fdd6c645f72eaf8ee1bce873ee437ed9b2f447d0
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-05-29 11:58:30 +0200

    WIP: neon server start

---
+++ lib/options.nix
@@ -0,0 +1,8 @@
+{ lib, ... }:
+let
+  inherit (lib) mkEnableOption;
+in
+{
+  # Create an option which is enabled by default, in contrast to `mkEnableOption` ones which are disabled by default.
+  mkDisableOption = description: (mkEnableOption description) // { default = true; };
+}
