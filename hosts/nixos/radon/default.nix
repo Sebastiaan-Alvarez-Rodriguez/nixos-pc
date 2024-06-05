da257f9928aa9b382e97e2eae212fe5f4170e90d
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-06-05 14:08:47 +0200

    WIP: fixed base

--- hosts/nixos/radon/default.nix
+++ hosts/nixos/radon/default.nix
@@ -1,73 +1,62 @@
 { config, pkgs, ... }: {
   imports = [
     ./hardware.nix
-  #   ./profiles.nix # seb todo
-  #   ./programs.nix # seb todo
-  #   ./secrets # seb todo
+    # ./profiles.nix # seb todo
+    # ./programs.nix # seb todo
+    # ./secrets # seb todo
   ];
 
-  # my.system.boot = {
-  #   enable = true;
-  #   tmp.clean = true;
-  #   kind = "systemd";
-  #   extraConfig = {
-  #     initrd.secrets = { "/crypto_keyfile.bin" = null; }; # Setup keyfile
-  #     kernelModules = [ "v4l2loopback" ];
-  #     extraModulePackages = [ config.boot.kernelPackages.v4l2loopback.out ];
-  #     extraModprobeConfig = ''
-  #       options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-  #     '';
-  #     supportedFilesystems = [ "ntfs" ]; # Allow NTFS reading https://nixos.wifi/wiki/NTFS
-  #     binfmt.emulatedSystems = [ "aarch64-linux" ];
-  #   };
-  # };
+  my.system.boot = {
+    enable = true;
+    tmp.clean = true;
+    kind = "systemd";
+    extraConfig = {
+      initrd.secrets = { "/crypto_keyfile.bin" = null; }; # Setup keyfile
+      kernelModules = [ "v4l2loopback" ];
+      extraModulePackages = [ config.boot.kernelPackages.v4l2loopback.out ];
+      extraModprobeConfig = ''
+        options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+      '';
+      supportedFilesystems = [ "ntfs" ]; # Allow NTFS reading https://nixos.wifi/wiki/NTFS
+      binfmt.emulatedSystems = [ "aarch64-linux" ];
+    };
+  };
 
-  # my.home = { # seb: TODO remove all unneeded packages from /modules/home. Especially watch out for pkgs guarded by mkDisableOption's, since they are by default enabled
-  #   bat.enable = true; # like cat, but with syntax highlighting & more
-  #   # bitwarden = {
-  #   #   enable = true;
-  #   #   pinentry = pkgs.pinentry-gtk2; # Use graphical pinentry  
-  #   #   mail = //... wait what? Should this not be different per-user?
-  #   # };
+  my.system = { # contains common system packages and settings shared between hosts.
+    nix.enable = true;
+    packages.enable = true;
+    packages.allowUnfree = true;
+  };
+
+  my.home = { # seb: TODO remove all unneeded packages from /modules/home. Especially watch out for pkgs guarded by mkDisableOption's, since they are by default enabled
+    bat.enable = true; # like cat, but with syntax highlighting & more
+    # bitwarden = {
+    #   enable = true;
+    #   pinentry = pkgs.pinentry-gtk2; # Use graphical pinentry  
+    #   mail = //... wait what? Should this not be different per-user?
+    # };
     
-  #   firefox.enable = true;
-  #   firefox.tridactyl.enable = true; # seb: An arcane way to use firefox
-  #   gpg.enable = true;
-  #   # gpg.pinentry = pkgs.pinentry-gtk2; # Use a small popup to enter passwords
+    firefox.enable = true;
+    firefox.tridactyl.enable = true; # seb: An arcane way to use firefox
+    gpg.enable = true;
+    # gpg.pinentry = pkgs.pinentry-gtk2; # Use a small popup to enter passwords
 
-  #   # packages.additionalPackages = with pkgs; [
-  #   #   jellyfin-media-player # Wraps the webui and mpv together
-  #   # ];
-  #   # mpv.enable = true; # Minimal video player
-  # };
+    # packages.additionalPackages = with pkgs; [
+    #   jellyfin-media-player # Wraps the webui and mpv together
+    # ];
+    # mpv.enable = true; # Minimal video player
+  };
 
-  # my.services = {
+  # my.services = { # seb: TODO uncomment after handling wireguard config.
   #   wireguard.enable = true;
   # };
 
   # seb: TODO continue from here to bottom
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-  i18n.defaultLocale = "en_US.utf8";
-
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk];
-    config.common.default = "*";
-  };
-
-  # virtualisation.docker.enable = true;
-
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
   
   programs = {
     adb.enable = true; # To use, users must be added to the "adbusers" group
-    fish.enable = true;
     steam = {
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -75,6 +64,13 @@
     dconf.enable = true;
     nix-ld.enable = true; # for all those executables with hardcoded /lib64 dynamic linker
     xwayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk];
+    config.common.default = "*";
   };
 
   services = {
@@ -118,6 +114,9 @@
     shell = pkgs.fish;
     password = "changeme";
   };
+
+  time.timeZone = "Europe/Amsterdam";
+  i18n.defaultLocale = "en_US.utf8";
 
   system.stateVersion = "23.11"; # Do not change
 }
