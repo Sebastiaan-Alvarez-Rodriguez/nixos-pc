16b9917391d0a1ef228c1c42dc2b9121c2321a35
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-06-19 23:08:33 +0200

    WIP: basic improvements

--- modules/home/wm/wpaperd/default.nix
+++ modules/home/wm/wpaperd/default.nix
@@ -8,15 +8,23 @@ in {
         message = "wpaperd module requires wayland graphics manager (set my.home.gm.manager = \"wayland\")";
       }
     ];
+
     programs.wpaperd = {
       enable = true;
       settings = {
         default = {
-          path = "../../res/background"; # seb: NOTE was absolute path: "${config.home.homeDirectory}/Pictures/Wallpapers"
+          path = ../../res/background; # seb: NOTE was absolute path: "${config.home.homeDirectory}/Pictures/Wallpapers"
           duration = "30m";
           apply-shadow = true;
           sorting = "random";
         };
+      };
+    };
+
+    systemd.user.services.wpaperd = { # NOTE user services: https://haseebmajid.dev/posts/2023-10-08-how-to-create-systemd-services-in-nix-home-manager/
+      Install.after = [ "river-session.target" ]; # seb: TODO used to be just 'after'
+      serviceConfig = {
+        # ExecStart = "${pkgs.strip-dkim}/bin/strip-dkim";
       };
     };
   };
