30b1d04e85cb70d5f1b2513f5b207132fa8a4b21
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-07-08 20:03:56 +0200

    Flake parts configuration

--- flake/nixos.nix
+++ flake/nixos.nix
@@ -11,9 +11,8 @@
     inherit system;
     modules = defaultModules ++ [ "${self}/hosts/nixos/${name}" ];
     specialArgs = {
-      inherit (self) lib; # Use my extended lib in NixOS configuration
-      inherit inputs; # Inject inputs to use them in global registry
-      inherit system;
+      inherit (self) lib; # use custom lib when configuring.
+      inherit inputs system;
     };
   };
 in {
@@ -24,28 +23,3 @@ in {
   "xenon" = "x86_64-linux";
   };
 }
-
-# # host definitions for flake.
-# { self, inputs, lib, nixpkgs, ... }: let
-#   defaultModules = [
-#     {
-#       system.configurationRevision = self.rev or "dirty"; # Let 'nixos-version --json' know about the Git revision
-#       nixpkgs.overlays = [ inputs.nur.overlay ]; # seb: NOTE used to be: nixpkgs.overlays = (nixpkgs.lib.attrValues self.overlays) ++ [ inputs.nur.overlay ];
-#     }
-#     "${self}/modules/nixos" # Include generic settings
-#   ];
-#   buildHost = name: system: nixpkgs.lib.nixosSystem {
-#     inherit system;
-#     modules = defaultModules ++ [ "${self}/hosts/nixos/${name}" ];
-#     # pkgs = inputs.nixpkgs.outputs.legacyPackages.${system};
-#     specialArgs = {
-#       inherit nixpkgs lib inputs system; # Inject inputs to use them in global registry
-#     };
-#   };
-# in nixpkgs.lib.mapAttrs buildHost {
-#   # "blackberry" = "aarch64-linux";
-#   "helium" = "x86_64-linux";
-#   "polonium" = "x86_64-linux";
-#   "radon" = "x86_64-linux";
-#   "xenon" = "x86_64-linux";
-# }
--- flake/packages.nix
+++ flake/packages.nix
@@ -1,11 +1,5 @@
 { self, inputs, ... }: {
   perSystem = { pkgs, system, ... }: {
-    packages = let
-      # inherit (inputs.futils.lib) filterPackages flattenTree;
-      packages = import "${self}/pkgs" { inherit pkgs; };
-      # flattenedPackages = flattenTree packages;
-      # finalPackages = filterPackages system flattenedPackages;
-    in
-      packages;
+    packages = import "${self}/pkgs" { inherit pkgs; };
   };
 }
