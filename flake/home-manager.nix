8277688c4dd48bc09fbd0d980321a9dd008f3430
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-07-08 20:03:56 +0200

    Flake parts configuration

--- flake/home-manager.nix
+++ flake/home-manager.nix
@@ -1,4 +1,4 @@
-# host definitions for flake.
+# user-home definitions for flake.
 { self, inputs, lib, nixpkgs, ... }: let
   mkHome = { system, userModule }: inputs.home-manager.lib.homeManagerConfiguration {
     modules = [
@@ -8,7 +8,6 @@
           overlays = [ self.overlays.default self.overlays.customlib self.overlays.custompkgs ];
           config = {
             allowUnfree = true;
-            # https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
             allowUnfreePredicate = (pkg: true);
           };
         };
@@ -22,33 +21,12 @@
     extraSpecialArgs = { inherit inputs; };
   };
 in {
-  headless = mkHome {
-    system = "x86_64-linux";
-    userModule = ../hosts/homes/headless.nix;
+  perSystem = { system, ... }: {
+    legacyPackages.homeConfigurations = {
+      headless = mkHome {
+        system = "x86_64-linux";
+        userModule = ../hosts/homes/headless.nix;
+      };
+    };
   };
 }
-# # host definitions for flake.
-# { self, inputs, lib, nixpkgs, ... }: let
-#   mkHome = { system, userModule }: home-manager.lib.homeManagerConfiguration {
-#     modules = [
-#       userModule
-#       {
-#         nixpkgs = {
-#           overlays = [ self.overlays.default self.overlays.customlib self.overlays.custompkgs ];
-#           config = {
-#             allowUnfree = true;
-#             # https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
-#             allowUnfreePredicate = (pkg: true);
-#           };
-#         };
-#       }
-#     ];
-#     pkgs = nixpkgs.outputs.legacyPackages.${system};
-#     extraSpecialArgs = { inherit inputs; };
-#   };
-# in {
-#   headless = mkHome {
-#     system = "x86_64-linux";
-#     userModule = ${self}/hosts/homes/headless.nix;
-#   };
-# };
--- flake/overlays.nix
+++ flake/overlays.nix
@@ -3,12 +3,8 @@
 
   additional-overlays = {
     lib = _final: _prev: { inherit (self) lib; }; # Expose expanded library
-
-    pkgs = _final: prev: { # Expose custom packages
-      custompkgs = prev.recurseIntoAttrs (import "${self}/pkgs" { pkgs = prev; });
-    };
+    pkgs = _final: prev: { custompkgs = prev.recurseIntoAttrs (import "${self}/pkgs" { pkgs = prev; }); }; # Expose custom packages
   };
-in
-{
+in {
   flake.overlays = default-overlays // additional-overlays;
 }
