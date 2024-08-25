f99435a310ed29e17eabdd5e8dc4951cda65aecf
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2024-08-25 20:22:40 +0200

    WIP: laptop config

--- modules/home/spotify/default.nix
+++ modules/home/spotify/default.nix
@@ -2,10 +2,12 @@
   spicetify-nix = inputs.spicetify-nix.outputs.packages.x86_64-linux; # seb: TODO change hardcoded system here
   # spicePkgs = inputs.spicetify-nix.outputs.packages.${pkgs.system}.default;
   # spicePkgs = inputs.spicetify-nix.outputs.packages;
-  spicePkgs = inputs.spicetify-nix.outputs.packages.x86_64-linux.default;
+  # spicePkgs = inputs.spicetify-nix.outputs.packages.x86_64-linux.default;
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
   cfg = config.my.home.spotify;
 in {
-  imports = [ inputs.spicetify-nix.outputs.homeManagerModule ];
+  # imports = [ inputs.spicetify-nix.outputs.homeManagerModule ];
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
   options.my.home.spotify = with lib; {
     enable = mkEnableOption "Spotify";
@@ -15,6 +17,9 @@ in {
     enable = true;
     theme = spicePkgs.themes.catppuccin;
     # theme = spicePkgs.themes.SpotifyNoPremium; //Same as default spotify but without ads and anything related to getting premium
-    enabledExtensions = with spicePkgs.extensions; [adblock];
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      hidePodcasts
+    ];
   };
 }
