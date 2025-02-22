6be9ddeadc9883acefc0f433f88d3627263a323d
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2025-02-22 22:43:22 +0100

    WIP: hd-idle

--- hosts/nixos/blackberry/hardware.nix
+++ hosts/nixos/blackberry/hardware.nix
@@ -4,12 +4,17 @@
     (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
   ];
 
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi3;
   boot.initrd.availableKernelModules = lib.mkOverride 0 [ ];
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi3;
+  # boot.kernelParams = [ "usbcore.autosuspend=-1" ]; # do not suspend USBs.
   boot.supportedFilesystems = lib.mkOverride 0 [ ];
 
   sdImage.compressImage = false;
 
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/feb8e53b-b7f3-40dc-b2c2-f947226bb278";
+    fsType = "ext4";
+  };
 
   my.hardware.networking = {
     enable = true;
