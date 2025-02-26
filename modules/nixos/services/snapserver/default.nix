25984a62e6884f06ce5811c4e4774ab873c2b1c9
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2025-02-26 22:19:49 +0100

    WIP: nginx, music-assistant, snapserver

--- modules/nixos/services/snapserver/default.nix
+++ modules/nixos/services/snapserver/default.nix
@@ -2,18 +2,75 @@
 { config, lib, ... }: let
   cfg = config.my.services.snapserver;
 in {
-  options.my.services.snapserver = {
-    enable = lib.mkEnableOption "snapserver Media Server";
+  options.my.services.snapserver = with lib; {
+    enable = mkEnableOption "snapserver Media Server";
+
+    port = mkOption {
+      type = types.port;
+      default = 5778;
+      description = "Port for snapclients to listen on";
+    };
+
+    listen-address = mkOption {
+      type = types.str;
+      default = "0.0.0.0";
+      description = "Interface to listen for snapclients";
+    };
+
+    codec = mkOption {
+      type = with types; nullOr enum [ "pcm" "flac" "vorbis" "opus" ];
+      default = "flac";
+      description = "Default audio compression method from server to clients.";
+    };
+
+    json-rpc = {
+      tcp = {
+        enable = mkEnableOption "snapserver JSON RPC over TCP";
+        port = mkOption {
+          type = types.port;
+          default = 5776;
+          description = "Internal port for JSON RPC over TCP";
+        };
+      };
+
+      http = {
+        enable = mkEnableOption "snapserver JSON RPC over HTTP";
+        port = mkOption {
+          type = types.port;
+          default = 5777;
+          description = "Internal port for JSON RPC over HTTP";
+        };
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.snapserver = {
       enable = true;
-      group = "media";
+      port = cfg.port;
+      listenAddress = cfg.listen-address;
+
+      tcp = {
+        inherit (cfg.json-rpc.tcp) enable port;
+        listenAddress = "127.0.0.1";
+      };
+      
+      http = {
+        inherit (cfg.json-rpc.http) enable port;
+        listenAddress = "127.0.0.1";
+      };
     };
 
-    my.services.nginx.virtualHosts.snapserver = {
-      port = 8096;
+    networking.firewall = {
+      allowedTCPPorts = [ cfg.port ] ++ lib.optional cfg.json-rpc.tcp.enable cfg.json-rpc.tcp.port; # no need to enable json-rpc.http.port, we just setup nginx to passthrough below if enabled.
+      extraCommands = ''
+        iptables -A INPUT -p tcp --dport ${toString cfg.port} -j DROP
+        iptables -A INPUT -p tcp --dport ${toString cfg.json-rpc.tcp.port} -j DROP
+      '';
+    };
+
+    my.services.nginx.virtualHosts.snapserver = lib.mkIf cfg.json-rpc.http.enable {
+      port = cfg.json-rpc.http.port;
       extraConfig = {
         locations."/" = {
           extraConfig = ''
@@ -22,7 +79,7 @@ in {
         };
         # Too bad for the repetition...
         locations."/socket" = {
-          proxyPass = "http://127.0.0.1:8096/";
+          proxyPass = "http://127.0.0.1:${toString cfg.json-rpc.http.port}/";
           proxyWebsockets = true;
         };
       };
