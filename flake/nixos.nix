f084285947d0ab5e586c6db8b691fbdb729af31e
Sebastiaan-Alvarez-Rodriguez <sebastiaanalva@gmail.com>
2025-03-28 23:21:39 +0100

    WIP: expose service modules with nesting

--- flake/nixos.nix
+++ flake/nixos.nix
@@ -49,20 +49,16 @@ in rec {
   # perSystem = { inputs', system, ...}: {
     # extraSpecialArgs = { inherit inputs' system;};
 
-  flake.nixosModules = let
+  flake.nixosModules.nixos-pc = let
     # nested-paths = (import ../modules/nixos {inherit lib system;}).imports;
     # unnest-import = path: (import path { }).imports;
     # import-paths = paths: lib.flatten (builtins.map unnest-import paths); # --> [ [./something ./other] [./hi ]} ] --> [ ./something ./other ./hi ...]
     # final-paths = import-paths nested-paths;
-    nested-paths = (import ../modules/nixos/services {inherit lib;}).imports;
-    final-paths = nested-paths;
+    final-paths = (import ../modules/nixos/services {inherit lib;}).imports;
     name-it = path: let
       parts = builtins.split "/" (toString path);
     in lib.nameValuePair (builtins.elemAt parts (builtins.length parts -1)) path;
   in lib.listToAttrs (builtins.map name-it final-paths);
-  # {
-  #   nixos-pc = lib.listToAttrs (builtins.map name-it final-paths);
-  # };
   # };
     # {jellyfin = import ../modules/nixos/services/jellyfin; }
   # flake.nixosModules = import ../modules/nixos { inherit lib; };
